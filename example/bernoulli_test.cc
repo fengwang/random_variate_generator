dc801fc61d9c666ae353a2261642f3fb4c7a31c0
wang feng <wanng.fenng@gmail.com>
2010-11-02 08:25:16 +0800

    bernoulli and laplace test case added.

---
+++ example/bernoulli_test.cc
@@ -0,0 +1,24 @@
+#include <vg.hpp>
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+
+using namespace vg;
+using namespace std;
+
+int main()
+{
+    variate_generator<int, bernoulli> vg(0.33);    
+
+    copy( vg.begin(), vg.begin()+1000, ostream_iterator<int>(cout, "\n"));
+
+
+    auto sum = accumulate( vg.begin(), vg.begin()+1000, 0 );
+
+    cout << sum << endl;
+
+    return 0;
+}
+
