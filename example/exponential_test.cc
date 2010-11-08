ddfa1af6b67de884c5c52fddbbe9933afce040a2
wang feng <wanng.fenng@gmail.com>
2010-11-09 02:15:59 +0800

    exponential test case added.

---
+++ example/exponential_test.cc
@@ -0,0 +1,23 @@
+#include <vg.hpp>
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+
+using namespace vg;
+using namespace std;
+
+int main()
+{
+    variate_generator<double, exponential> vg(10);    
+
+    copy( vg.begin(), vg.begin()+1000, ostream_iterator<double>(cout, "\n"));
+
+    auto sum = accumulate( vg.begin(), vg.begin()+1000, 0.0 );
+
+    cout << sum << endl;
+
+    return 0;
+}
+
