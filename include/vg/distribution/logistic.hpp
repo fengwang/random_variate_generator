26f9382593a67daf97cd7788c4c409e7e9144667
wang feng <wanng.fenng@gmail.com>
2010-11-23 07:06:17 +0800

    logistic distribution added.

---
+++ include/vg/distribution/logistic.hpp
@@ -0,0 +1,64 @@
+#ifndef _LOGISTIC_HPP_INCLUDED_SDOIU4398YASFKLJH3498YAFSDOIU34UIOAHSFDKLMNSFDKJH3IUHASFDAFLKJ
+#define _LOGISTIC_HPP_INCLUDED_SDOIU4398YASFKLJH3498YAFSDOIU34UIOAHSFDKLMNSFDKJH3IUHASFDAFLKJ 
+
+#include <vg/distribution/beta.hpp>
+
+#include <cmath>
+#include <cassert>
+
+namespace vg
+{
+
+    template <   typename Return_Type,
+                 typename Engine
+             >
+    struct logistic : private beta<Return_Type, Engine>
+    {
+            typedef beta<Return_Type, Engine>           beta_type;
+            typedef Return_Type                         return_type;
+            typedef Engine                              engine_type;
+            typedef typename engine_type::final_type    final_type;
+            typedef typename engine_type::seed_type     seed_type;
+
+            return_type         a_;
+            return_type         b_;
+            engine_type         e_;
+
+            explicit logistic(	const return_type a = 1,
+                                const return_type b = 1,
+                                const seed_type sd = 0 )
+                : a_( a ), b_(b), e_( sd )
+            {
+                assert( a > 0 );
+                assert( b > 0 );
+            }
+
+            return_type
+            operator()()
+            {
+                return do_generation( a_, b_ );
+            }
+
+        protected:
+            return_type
+            do_generation( const final_type A, const final_type B )
+            {
+                return beta_impl( A, B );
+            }
+
+        private:
+            return_type
+            beta_impl( const final_type A, const final_type B )
+            {
+                const final_type X = beta_type::do_generation( A, B );
+                const final_type ans = std::log( X / ( final_type(1) - X ) );
+                return ans;
+            }
+
+    };
+
+}//vg
+
+
+#endif//_LOGISTIC_HPP_INCLUDED_SDOIU4398YASFKLJH3498YAFSDOIU34UIOAHSFDKLMNSFDKJH3IUHASFDAFLKJ 
+
