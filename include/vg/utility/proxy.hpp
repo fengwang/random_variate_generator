eecfcaf0cbde3e4e708546413ee469aedd2acd0d
wang feng <wanng.fenng@gmail.com>
2010-11-24 03:12:23 +0800

    proxy wrapper enhanced.

--- include/vg/utility/proxy.hpp
+++ include/vg/utility/proxy.hpp
@@ -1,11 +1,24 @@
 #ifndef _PROXY_HPP_INCLUDED_SDOIFJ438OUAFSLSF988923PFUISLFKJEOIJFLKJDOIJFALIJEIJ
 #define _PROXY_HPP_INCLUDED_SDOIFJ438OUAFSLSF988923PFUISLFKJEOIJFLKJDOIJFALIJEIJ
 
+#include <cstddef>
+
 namespace vg 
 {
 
-template<typename T>
-struct proxy : T
+//template<typename T>
+//struct proxy : T
+//{};
+
+template< typename T, std::size_t Level = 0 >
+struct proxy;
+
+template< typename T, std::size_t Level >
+struct proxy : proxy< T, Level - 1 >
+{};
+
+template< typename T >
+struct proxy<T, 0> : T
 {};
 
 
