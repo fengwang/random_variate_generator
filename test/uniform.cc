627b0582608a62f861adc1432693c24037cfe006
Feng Wang <feng.wang@uni-ulm.de>
2014-09-15 18:37:28 +0200

    add

--- test/uniform.cc
+++ test/uniform.cc
@@ -5,7 +5,8 @@
 
 int main()
 {
-    vg::variate_generator<double, vg::uniform, vg::mt19937> vg_(-1, 1);
+    //vg::variate_generator<double, vg::uniform, vg::mt19937> vg_(-1, 1);
+    vg::variate_generator<double, vg::uniform, vg::lagged_fibonacci> vg_(-1, 1);
 
     std::size_t const n = 10000000;
     std::vector<double> x(n);
